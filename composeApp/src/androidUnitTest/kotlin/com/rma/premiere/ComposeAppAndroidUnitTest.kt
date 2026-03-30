0e158f26718d99c2caa2dbba7c959314a51be6f9
Mihajlo1612 <mradojevic016@gmail.com>
2026-03-30 23:46:33 +0200

    Initial commit - Premiere project setup (KMP)

---
+++ composeApp/src/androidUnitTest/kotlin/com/rma/premiere/ComposeAppAndroidUnitTest.kt
@@ -0,0 +1,12 @@
+package com.rma.premiere
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppAndroidUnitTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
